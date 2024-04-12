fe71a7492f52b03aeaf67b134624c440412c9ba5
Gil <gilbertjvirgo@gmail.com>
2024-04-12 10:25:37 +0100

    Added back-to-progress page button

--- response/success.js
+++ response/success.js
@@ -1,2 +1,14 @@
-export default /*html*/ `<h1>Success</h1>
-	<p>You can now close this tab</p>`;
+const html = String.raw;
+
+export default html`<h1>Success</h1>
+	<p>You can now close this tab</p>
+	<p>
+		<button type="button" onclick="goToProgressPage()">
+			Back to my progress
+		</button>
+	</p>
+	<script>
+		const goToProgressPage = () =>
+			(window.location.href =
+				"/" + window.location.pathname.split("/")[1] + "/progress");
+	</script>`;
