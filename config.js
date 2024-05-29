4896dd9e4a5ea8795cc29f5120ee1efb1f4a1ccc
Gil <gilbertjvirgo@gmail.com>
2024-05-29 07:14:52 +0100

    Persistent columns now only Pentateuch and Paul

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const scale = 2000,
 	gutter = 0.02;
 
 export default {
-	persistentColumns: [0, 1, 2], // These chapters will always advance and will not need to be marked as read
+	persistentColumns: [0, 1], // These chapters will always advance and will not need to be marked as read
 	printDimensions: {
 		width: Math.sqrt(2) * 2000,
 		height: scale,
