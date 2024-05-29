e70687e0a67e5fde1bed213253c658ecf55eb8e8
Gil <gilbertjvirgo@gmail.com>
2024-05-29 07:00:57 +0100

    Adjusted plan to Ben Skaug's plan

--- plan.js
+++ plan.js
@@ -1,4 +1,5 @@
 export default [
+	// Pentateuch
 	[
 		["Genesis", 50],
 		["Exodus", 40],
@@ -6,16 +7,8 @@ export default [
 		["Numbers", 36],
 		["Deuteronomy", 34],
 	],
-	[["Psalms", 150]],
-	[
-		["Romans", 16],
-		["1 Corinthians", 16],
-		["2 Corinthians", 13],
-		["Galatians", 6],
-		["Ephesians", 6],
-		["Philippians", 4],
-		["Colossians", 4],
-	],
+
+	// History
 	[
 		["Joshua", 24],
 		["Judges", 21],
@@ -24,28 +17,29 @@ export default [
 		["2 Samuel", 24],
 		["1 Kings", 22],
 		["2 Kings", 25],
-	],
-	[
 		["1 Chronicles", 29],
 		["2 Chronicles", 36],
 		["Ezra", 10],
 		["Nehemiah", 13],
 		["Esther", 10],
 	],
-	[["Proverbs", 31]],
+
+	// Poetry
 	[
 		["Job", 42],
+		["Psalms", 150],
+		["Proverbs", 31],
 		["Ecclesiastes", 12],
 		["Song of Solomon", 8],
 	],
+
+	// Prophets
 	[
 		["Isaiah", 66],
 		["Jeremiah", 52],
 		["Lamentations", 5],
 		["Ezekiel", 48],
 		["Daniel", 12],
-	],
-	[
 		["Hosea", 14],
 		["Joel", 3],
 		["Amos", 9],
@@ -59,20 +53,35 @@ export default [
 		["Zechariah", 14],
 		["Malachi", 4],
 	],
+
+	// Gospels + Acts
 	[
 		["Matthew", 28],
 		["Mark", 16],
 		["Luke", 24],
 		["John", 21],
+		["Acts", 28],
 	],
-	[["Acts", 28]],
+
+	// Paul
 	[
+		["Romans", 16],
+		["1 Corinthians", 16],
+		["2 Corinthians", 13],
+		["Galatians", 6],
+		["Ephesians", 6],
+		["Philippians", 4],
+		["Colossians", 4],
 		["1 Thessalonians", 5],
 		["2 Thessalonians", 3],
 		["1 Timothy", 6],
 		["2 Timothy", 4],
 		["Titus", 3],
 		["Philemon", 1],
+	],
+
+	// Everything else
+	[
 		["Hebrews", 13],
 		["James", 5],
 		["1 Peter", 5],
@@ -81,6 +90,6 @@ export default [
 		["2 John", 1],
 		["3 John", 1],
 		["Jude", 1],
+		["Revelation", 22],
 	],
-	[["Revelation", 22]],
 ];
